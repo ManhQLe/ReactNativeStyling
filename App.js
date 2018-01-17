20d838aab7cad74ea4456ebb2f27c0e20556d91a
Manh Le <quang.manh@gmail.com>
2018-01-16 21:35:32 -0600

    Finished Stack navigation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, AppRegistry } from 'react-native'
+import { StyleSheet, Text, View, AppRegistry, TouchableOpacity } from 'react-native'
 import styled from 'styled-components/native'
 
 import {FontAwesome, Ionicons}  from '@expo/vector-icons'
@@ -9,14 +9,23 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		justifyContent: "center",
-		alignItems: "center",
-		flexDirection: "row"
+		backgroundColor:"#34495e"
 	},
 	box: {
 		height: 50,
 		width: 50,
 		margin: 5,
 		backgroundColor: '#e76e63'
+	},
+	text:{		
+		fontSize:30
+	},
+	btn:{
+		padding:10,
+		borderRadius:5,
+		backgroundColor:"#e74c3c",	
+		justifyContent:"center",
+		height:50
 	}
 })
 
@@ -37,18 +46,49 @@ function Dashboard(){
 	)
 }
 
+function Home2({navigation}){
+	return(
+		<View style={styles.container}>
+			<Text style={styles.text}>HOME VIEW</Text>
+			<TouchableOpacity style={styles.btn} onPress={()=>navigation.navigate('Dashboard')}>
+				<Text>To Dashboard</Text>
+			</TouchableOpacity>
+		</View>
+	)
+}
+
 const Tabs = TabNavigator({
 	Home:{
 		screen:Home,
 		navigationOptions:{
 			tabBarIcon:()=> <FontAwesome name='home' size={30} color='black'/>
-		}		
+		}	
 	},
 	Dashboard:{
 		screen:Dashboard,
 		navigationOptions:{
 			tabBarIcon:()=> <FontAwesome name='dashboard' size={30} color='black'/>
 		}
+	}
+})
+
+const Stack = StackNavigator({
+	Home:{
+		screen:Home2,
+		navigationOptions:{
+			title:"Home"
+		}
+	},
+	Dashboard:{
+		screen:Dashboard,
+		navigationOptions:{
+			title:"Home",
+			headerTintColor:"#ecf0f1",
+			headerStyle:{
+				backgroundColor:"#e67e22"
+			}
+
+		}	
 	}
 })
 
@@ -77,7 +117,7 @@ class FlexboxExamples extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Tabs/>
+				<Stack/>
 			</View>
 		)
 	}
